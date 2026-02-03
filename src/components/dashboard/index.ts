501bbb0a8222629a58ff08360ca04f451f015485
obaidullah-miazi-dev <obaidullahmiazi.dev@gmail.com>
2026-02-03 18:29:58 +0600

    feat: dashboard components export index

---
+++ src/components/dashboard/index.ts
@@ -0,0 +1,6 @@
+export { KPICard } from './KPICard';
+export { RevenueChart } from './RevenueChart';
+export { SalesChart } from './SalesChart';
+export { CategoryChart } from './CategoryChart';
+export { GrowthChart } from './GrowthChart';
+export { RecentOrdersTable } from './RecentOrdersTable';
